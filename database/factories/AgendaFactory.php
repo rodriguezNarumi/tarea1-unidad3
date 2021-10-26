9d05d87eb0684b1cddc75cadc761f22c06dbd2bb
narumi <narumirodriguez@gmail.com>
2021-10-25 21:58:30 -0500

    tarea1

---
+++ database/factories/AgendaFactory.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Agenda;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+class AgendaFactory extends Factory
+{
+    /**
+     * The name of the factory's corresponding model.
+     *
+     * @var string
+     */
+    protected $model = Agenda::class;
+
+    /**
+     * Define the model's default state.
+     *
+     * @return array
+     */
+    public function definition()
+    {
+        return [
+            'nombre' => $this->faker->name(),
+            'telefono' => $this->faker->phoneNumber(),
+            'email' => $this->faker->email()
+        ];
+    }
+}
---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,23 @@
+
+
+<h1>bienvenidos al formulario</h1>
+
+<form action="">
+    <label>
+        nombre:
+        <br>
+        <input type="text" name="name">
+</label>
+<br>
+<label>
+        descripcion:
+        <br>
+        <textarea name="descripcion" rows="5"></textarea>
+</label>
+<br>
+<label>
+    categoria:
+    <br>
+    <input type="text" name="categoria">
+</label>
+</form>
